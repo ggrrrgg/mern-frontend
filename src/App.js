8fe7b39f406d2ec7d9d0a3e5ef809f4dae0b23c7
ggrrrgg <126563391+ggrrrgg@users.noreply.github.com>
2023-11-09 21:02:21 +1100

    comp

---
+++ src/App.js
@@ -0,0 +1,58 @@
+
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+
+  const [jwt, setJwt] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    console.log(`JWT value is:\n${jwt}`);
+  }, [jwt])
+
+  async function login(){ 
+    console.log(username, password);
+
+    let result = await fetch(
+      process.env.REACT_APP_BACKEND_URL + "/users/login",
+      {
+        method: "POST", // or 'PUT'
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({username: username, password: password}),
+      }
+    );
+
+    let data = await result.json();
+
+    console.log(data);
+
+  }
+
+  
+
+  return (
+    <div className="App">
+      <label>Username</label>
+      <input type='text' 
+      name='usernameInput' 
+      id='usernameInput' 
+      value={(username)} 
+      onChange={(event) => setUsername(event.target.value)}/>
+
+      <label>Password</label>
+      <input type='text' 
+      name='passwordInput' 
+      id='passwordInput' 
+      value={(password)} 
+      onChange={(event) => setPassword(event.target.value)}/>
+
+      <button onClick={login}>Login</button>
+    </div>
+  );
+}
+
+export default App;
